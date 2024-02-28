f87a18b6af64f4f25a363c1686aa06fd90c36995
ALEJANDRO CAMLTASKS <c.a.m.l.123@hotmail.com>
2024-02-27 20:34:19 -0500

    otro commit

---
+++ lib/pages/bienvenida.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BienvenidaApp extends StatelessWidget {
+  const BienvenidaApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    
+  }
+}
